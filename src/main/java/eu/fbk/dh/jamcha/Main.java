eaa1a9bbed328aecea8c1fb9e7482759ba0642da
Alessio Palmero Aprosio <alessio@apnetwork.it>
2017-06-09 12:02:28 +0200

    First commit

---
+++ src/main/java/eu/fbk/dh/jamcha/Main.java
@@ -0,0 +1,17 @@
+package eu.fbk.dh.jamcha;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by alessio on 09/06/17.
+ */
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
