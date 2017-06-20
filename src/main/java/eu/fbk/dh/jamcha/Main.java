ddeedaddc24fe83342b93ab99605f54dfe85d745
dan92 <dan92@PC-DANIELE.DSL-N55U>
2017-06-20 19:02:38 +0200

    Update pom.xml: wrong fbk version dependencies (utils-core and utils-svm)

--- src/main/java/eu/fbk/dh/jamcha/Main.java
+++ src/main/java/eu/fbk/dh/jamcha/Main.java
@@ -6,12 +6,13 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by alessio on 09/06/17.
  */
-
-public class Main {
+public class Main
+{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
     }
 }
