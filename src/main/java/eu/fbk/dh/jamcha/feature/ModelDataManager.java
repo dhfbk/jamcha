00127b313c2d7d85e21fba8ec56671ec48d5e36c
Daniele <dan92d@libero.it>
2017-10-21 02:24:55 +0200

    CreateModel bug fix

--- src/main/java/eu/fbk/dh/jamcha/feature/ModelDataManager.java
+++ src/main/java/eu/fbk/dh/jamcha/feature/ModelDataManager.java
@@ -4,6 +4,7 @@ import eu.fbk.dh.jamcha.feature.FeaturesSchema.Line;
 import eu.fbk.utils.svm.Classifier;
 import eu.fbk.utils.svm.LabelledVector;
 import eu.fbk.utils.svm.Vector;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Create model, save and load model and all data that will be used in future sessions
@@ -21,10 +23,12 @@ public class ModelDataManager
 {
     private static final String FOLDER_ROOT = "Data";
 
+    @Nonnull
     private final List<Line> features;
     private final FeatureParameters parameters;
     private HashMap<Integer, String> tagsMap;
     private Classifier classifier;
+    private Path basePath;
 
     /**
      * Constructor
@@ -41,6 +45,18 @@ public class ModelDataManager
         this.parameters = featuresSchema.getFeatureParameters();
     }
 
+    /**
+     * Constructor
+     *
+     * @param features   all integrated features
+     * @param parameters
+     */
+    public ModelDataManager(@Nonnull List<Line> features, @Nonnull FeatureParameters parameters)
+    {
+        this.features = features;
+        this.parameters = parameters;
+    }
+
     public boolean createModel()
     {
         if (features == null)
@@ -53,20 +69,30 @@ public class ModelDataManager
         {
             List<LabelledVector> labVectors = new ArrayList<>(features.size());
             tagsMap = new HashMap<>(features.size());
-            HashSet<String> tagsInserted = new HashSet<>(tagsMap.size());
+            HashMap<String, Integer> tagIndex = new HashMap<>(tagsMap.size());
+
             for (Line line : features)
             {
                 String tag = line.getTag();
-                if (tag != null && tagsInserted.add(tag))
+                if (tag != null)
                 {
-                    //Create tags map
-                    counter ++;
-                    tagsMap.put(counter, line.getTag());
-
+                    int index;
+                    if ( ! tagIndex.containsKey(tag))
+                    {
+                        // Add tag to tags maps
+                        counter ++;
+                        index = counter;
+                        tagIndex.put(tag, counter);
+                        tagsMap.put(counter, tag);
+                    }
+                    else
+                    {
+                        index = tagIndex.get(tag);
+                    }
                     // Create labelledVector representing this line and add it to global list
                     Vector.Builder builder = Vector.builder();
-                    builder.set(line.getTag());
-                    LabelledVector vector = builder.build().label(counter);
+                    builder.set(line.getWords());
+                    LabelledVector vector = builder.build().label(index);
                     labVectors.add(vector);
                 }
             }
@@ -84,39 +110,70 @@ public class ModelDataManager
         return retval;
     }
 
+    @Nullable
+    public Classifier getClassifier()
+    {
+        return this.classifier;
+    }
+
+    @Nullable
+    public HashMap<Integer, String> getTagsIndexesMap()
+    {
+        return this.tagsMap;
+    }
+
     /**
-     * Save the model created using train file and all values that will be used in future sessions
      *
      * @param folderPath path of folder that will contain all files.
+     *
+     * @return
+     */
+    public boolean setPath(@Nonnull final Path folderPath)
+    {
+        this.basePath = validateBasePath(folderPath);
+        return this.basePath != null;
+    }
+
+    /**
+     * Save the model created using train file and all values that will be used in future sessions
      *
      * @throws IOException default
      * @see IOException
      */
-    public void saveTo(@Nonnull final Path folderPath) throws IOException
+    public void saveModel() throws IOException
     {
-        if (classifier != null)
+        if (classifier != null && basePath != null)
         {
-            Path correctPath = validateBasePath(folderPath);
             // Save model
-            classifier.writeTo(correctPath);
+            classifier.writeTo(basePath);
             // Save features parameters
-            parameters.saveTo(correctPath);
+            parameters.saveTo(basePath);
         }
     }
 
-    public static Classifier loadModel(Path folderPath) throws IOException
+    /**
+     * Save all features to specified path. To set path call "setPath"
+     *
+     * @throws IOException default
+     * @see IOException
+     */
+    public void saveFeatures() throws IOException
     {
-        Path correctPath = validateBasePath(folderPath);
-        return Classifier.readFrom(correctPath);
+        Path featuresPath = Paths.get(basePath.toString(), "Features.txt");
+        try (BufferedWriter writer = Files.newBufferedWriter(featuresPath))
+        {
+            for (Line line : this.features)
+            {
+                for (String feature : line.getWords())
+                {
+                    writer.append(feature).append(" ");
+                }
+                writer.append(line.getTag() != null ? line.getTag() : "null");
+            }
+        }
     }
 
-    public static FeatureParameters loadFeatureParameters(Path folderPath) throws IOException
-    {
-        Path correctPath = validateBasePath(folderPath);
-        return FeatureParameters.loadFrom(correctPath);
-    }
-
-    private static Path validateBasePath(final Path pathToCheck)
+    private static Path validateBasePath(@Nonnull final Path pathToCheck)
     {
         Path retval = Paths.get(pathToCheck.toString());
         // Build correct path
@@ -129,5 +186,33 @@ public class ModelDataManager
             retval = Paths.get(retval.toString(), FOLDER_ROOT);
         }
         return retval;
+    }
+
+    /**
+     * Load all saved values(model, etc)
+     */
+    public final static class Loader
+    {
+        private final Path baseLoadPath;
+
+        /**
+         * Constructor
+         *
+         * @param baseFolderPath path of folder that will contain all files.
+         */
+        public Loader(@Nonnull Path baseFolderPath)
+        {
+            this.baseLoadPath = validateBasePath(baseFolderPath);
+        }
+
+        public Classifier loadModel() throws IOException
+        {
+            return Classifier.readFrom(baseLoadPath);
+        }
+
+        public FeatureParameters loadFeatureParameters() throws IOException
+        {
+            return FeatureParameters.loadFrom(baseLoadPath);
+        }
     }
 }
